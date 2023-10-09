7f6e0d088cd4971b376b0a12faff3a3bf4ae4cbc
nireo <eemil.lehtikangas1@gmail.com>
2023-10-10 00:53:52 +0300

    fixed formatting

--- pikkugrad/tensor.py
+++ pikkugrad/tensor.py
@@ -1,6 +1,11 @@
 from typing import ClassVar
 
 
+class Func:
+    def __init__(self, device: str, *tensors: Tensor) -> None:
+        self.device = device
+
+
 # Tensor represents a multi-dimensional scalar array.
 class Tensor:
     no_grad: ClassVar[bool] = False
